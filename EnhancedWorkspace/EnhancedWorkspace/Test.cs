b87616e0f1a110f8f1fea24231b04fb0dcc82858
Stephen Klancher <smknight@gmail.com>
2018-07-27 15:49:04 -0700

    testing

---
+++ EnhancedWorkspace/EnhancedWorkspace/Test.cs
@@ -0,0 +1,31 @@
+﻿using Agility.Sdk.Model.Capture;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TotalAgility.Sdk;
+
+namespace EnhancedWorkspace
+{
+    public class Test
+    {
+
+        public void WriteByteArrayToFile(byte[] bytearray, string filePath)
+        {
+            File.WriteAllBytes(filePath, bytearray);
+        }
+
+        public void WriteIntArrayToFile(int[] bytearray, string filePath)
+        {
+            byte[] bytes = bytearray.SelectMany(BitConverter.GetBytes).ToArray();
+            File.WriteAllBytes(filePath, bytes.ToArray<byte>());
+        }
+        public void WriteLongArrayToFile(long[] bytearray, string filePath)
+        {
+            byte[] bytes = bytearray.SelectMany(BitConverter.GetBytes).ToArray();
+            File.WriteAllBytes(filePath, bytes.ToArray<byte>());
+        }
+    }
+}
